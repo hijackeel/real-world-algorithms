1e367a50633e26b2a31cc8dc5b39b30be1cfe585
Hijack Eel <hjkl@semicolonpipe.io>
2018-05-21 22:35:21 -0700

    Get rid of VLA

--- src/1.2-stock-span-lib-test.c
+++ src/1.2-stock-span-lib-test.c
@@ -6,7 +6,7 @@ static void test_stock_span()
 {
   static int quotes[] = {7, 11, 8, 6, 3, 8, 9};
   static size_t length = sizeof quotes / sizeof *quotes;
-  size_t spans[length];
+  size_t spans[7];
   stock_span(quotes, spans, length);
   static size_t output[] = {1, 2, 1, 1, 1, 4, 5};
   for (size_t i=0; i<length; i++)
