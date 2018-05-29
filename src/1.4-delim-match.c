932031eedd37efe15a23830afff5f55ec9adf2a8
Hijack Eel <hjkl@semicolonpipe.io>
2018-05-29 01:13:59 -0700

    Add delim-match REPL

---
+++ src/1.4-delim-match.c
@@ -0,0 +1,22 @@
+#include "1.4-delim-match-lib.h"
+#include <stdio.h>
+#include <string.h>
+
+#define BUFFER_SIZE 500
+
+int main()
+{
+  char buffer[BUFFER_SIZE];
+  while(1)
+  {
+    // Prompt for input.
+    printf("> ");
+    fgets(buffer, BUFFER_SIZE, stdin);
+
+    // Convert newline at end of user input to null terminator.
+    strtok(buffer, "\n");
+
+    // Display output.
+    printf("%d\n\n", delim_match(buffer, strlen(buffer)));
+  }
+}
