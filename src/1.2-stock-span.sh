f6295674fb4ac8811eaca2a3325d820ff088dee0
Hijack Eel <hjkl@semicolonpipe.io>
2018-05-29 19:15:06 -0700

    Rename stock-span-trial* files

--- src/1.2-stock-span.sh
+++ src/1.2-stock-span.sh
@@ -5,7 +5,7 @@ api_key=$1
 valgrind --tool=callgrind\
   --instr-atstart=no\
   --callgrind-out-file=callgrind.stockspan\
-  ./1.2-stock-span-trial GOOGL $api_key;
+  ./1.2-stock-span GOOGL $api_key;
 
 callgrind_annotate --auto=yes --inclusive=yes callgrind.stockspan.1 2>/dev/null\
   | grep -E 'stock_span_stack \(1x\)';
